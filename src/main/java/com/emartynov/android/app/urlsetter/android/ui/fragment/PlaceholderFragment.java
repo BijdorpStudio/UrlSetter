eede478736a975463c25ccf805ed5f5792c5325e
Eugen Martynov <eugene.martynov@booking.com>
2013-12-29 17:35:18 +0100

    Minor re-formatting

--- src/main/java/com/emartynov/android/app/urlsetter/android/ui/fragment/PlaceholderFragment.java
+++ src/main/java/com/emartynov/android/app/urlsetter/android/ui/fragment/PlaceholderFragment.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  * Copyright 2013-2014 @BijdorpStudio
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *    http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
 package com.emartynov.android.app.urlsetter.android.ui.fragment;
 
 import android.app.Activity;
@@ -24,8 +42,7 @@ public class PlaceholderFragment extends Fragment
     }
 
     @Override
-    public void onInflate ( Activity activity, AttributeSet attrs,
-                            Bundle savedInstanceState )
+    public void onInflate ( Activity activity, AttributeSet attrs, Bundle savedInstanceState )
     {
         super.onInflate( activity, attrs, savedInstanceState );
 
