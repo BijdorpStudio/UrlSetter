a7b1c5615d16f1df18dec290abf5bf8529d7b0f7
Eugen Martynov <eugene.martynov@booking.com>
2013-12-29 17:10:31 +0100

    Linkifyed twitter name

--- src/main/java/com/emartynov/android/app/urlsetter/android/ui/AboutActivity.java
+++ src/main/java/com/emartynov/android/app/urlsetter/android/ui/AboutActivity.java
@@ -1,9 +1,14 @@
 package com.emartynov.android.app.urlsetter.android.ui;
 
 import android.os.Bundle;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 import com.emartynov.android.app.urlsetter.R;
 import com.emartynov.android.app.urlsetter.service.Crashlytics;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.inject.Inject;
 
@@ -20,6 +25,28 @@ public class AboutActivity extends InjectedActivity
         crashlytics.start( this );
 
         setContentView( R.layout.activity_about );
+
+        linkifyTwitter();
+    }
+
+    private void linkifyTwitter ()
+    {
+        TextView tweetView = (TextView) findViewById( R.id.twitter_name );
+
+        Pattern atMentionPattern = Pattern.compile( "@([A-Za-z0-9_]+)" );
+        String atMentionScheme = "http://twitter.com/";
+
+        Linkify.TransformFilter transformFilter = new Linkify.TransformFilter()
+        {
+            //skip the first character to filter out '@'
+            public String transformUrl ( final Matcher match, String url )
+            {
+                String group = match.group( 1 );
+                return group;
+            }
+        };
+
+        Linkify.addLinks( tweetView, atMentionPattern, atMentionScheme, null, transformFilter );
     }
 
 }
