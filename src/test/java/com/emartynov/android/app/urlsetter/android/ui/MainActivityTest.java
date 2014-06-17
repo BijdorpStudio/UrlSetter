d3c9871a223fb9e1c82a95dc0932d0c254922b33
Eugen Martynov <emartynov@minddistrict.com>
2014-06-18 00:18:44 +0200

    Added first test for activity with Action Bar

---
+++ src/test/java/com/emartynov/android/app/urlsetter/android/ui/MainActivityTest.java
@@ -0,0 +1,31 @@
+package com.emartynov.android.app.urlsetter.android.ui;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.robolectric.Robolectric.buildActivity;
+
+@Config( emulateSdk = 18 )
+@RunWith( RobolectricTestRunner.class )
+public class MainActivityTest
+{
+    private MainActivity activity;
+
+    @Before
+    public void setUp()
+        throws Exception
+    {
+        activity = buildActivity( MainActivity.class ).create().get();
+    }
+
+    @Test
+    public void finishesAfterStart()
+        throws Exception
+    {
+        assertThat( activity.isFinishing() ).isFalse();
+    }
+}
